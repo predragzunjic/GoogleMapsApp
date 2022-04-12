339257c59d163d007c74d4bdd8e5e0fed9d66ce0
Pedja <pedja@gmail.com>
2022-04-12 20:48:25 +0200

    Hidden api key

--- app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
@@ -1,10 +1,18 @@
 package com.example.googlemapsapp
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.googlemapsapp.databinding.ActivityMapsBinding
+import com.example.googlemapsapp.models.Steps
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,12 +20,18 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-
+@AndroidEntryPoint
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private val mapsViewModel: MapsViewModel by viewModels()
+    private lateinit var listOfMarkers: MutableList<Marker?>
+    private lateinit var KEY_API: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +39,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        listOfMarkers = mutableListOf<Marker?>()
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["keyValue"]
+
+        KEY_API = value.toString()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -48,9 +68,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(podgorica, 14.0F))
 
+        initView(googleMap)
+
+
+    }
+
+    private fun initView(googleMap: GoogleMap){
+        var counter = 0
+
         var origin: Marker?
         var destination: Marker?
-        var counter = 0
 
         binding.btnMakeRoute.setOnClickListener{
             binding.btnMakeRoute.visibility = INVISIBLE
@@ -77,15 +104,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 // Placing a marker on the touched position
                 counter++
                 if(counter == 1)
-                    origin = googleMap.addMarker(markerOptions)
+                    listOfMarkers.add(googleMap.addMarker(markerOptions))
 
                 else if(counter == 2){
-                    destination = googleMap.addMarker(markerOptions)
+                    listOfMarkers.add(googleMap.addMarker(markerOptions))
                     counter = 0
+                    getDirections(listOfMarkers)
+                    listOfMarkers.clear()
                     binding.btnMakeRoute.visibility = VISIBLE
                 }
             }
         }
+    }
 
+    private fun getDirections(listOfMarkers: MutableList<Marker?>){
+        mapsViewModel.getDirections("DRIVING", true, KEY_API, "${listOfMarkers[0]?.title?.substringBefore(':')}," +
+                "${listOfMarkers[0]?.title?.substringBefore(':')}", "${listOfMarkers[1]?.title?.substringBefore(':')},"
+                + "${listOfMarkers[1]?.title?.substringBefore(':')}")
+
+        lifecycleScope.launchWhenStarted {
+            mapsViewModel.directions.collect{event ->
+                when(event){
+                    is MapsViewModel.DirectionsEvent.Success ->{
+                        createPolyline(event.resultText)
+                    }
+
+                    is MapsViewModel.DirectionsEvent.Failure ->{
+
+                    }
+
+                    is MapsViewModel.DirectionsEvent.Loading ->{
+
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun createPolyline(steps: List<Steps>?) {
+        if (!steps.isNullOrEmpty()) {
+            for (it in steps) {
+                mMap.addPolyline(
+                    PolylineOptions()
+                        .add(LatLng(it.start_location?.latitude!!.toDouble(), it.start_location?.longitude!!.toDouble())
+                            , LatLng(it.end_location?.latitude!!.toDouble(), it.end_location?.longitude!!.toDouble()))
+                        .width(5f)
+                        .color(Color.RED)
+                )
+            }
+        }else{
+            Toast.makeText(this@MapsActivity, "There is a problem with creating an interface",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/googlemapsapp/util/Constants.kt
+++ app/src/main/java/com/example/googlemapsapp/util/Constants.kt
@@ -1,9 +1,10 @@
 package com.example.googlemapsapp.util
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+
 class Constants {
     companion object{
-        const val KEY_API = "AIzaSyCtd1p67tdHSJE6amM78wo5ZNy4s6Mxnx4"
-
         const val BASE_URL = "https://maps.googleapis.com/maps/api/"
     }
 }
